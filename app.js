3752903b9d9f6947acc716c20817675096a69fee
fahadkhan917 <fk16181@gmail.com>
2020-07-24 08:46:25 +1000

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,25 @@ var web={
 //propertie:value    
     t1:"ali",
     t2:"fahad",
-    t3:"asad"
+    t3:"asad",
+    t5:['umair','zain']
 };
+//add a t4 like this
+web.t4="umair";
+web.t6=['huma','areeb'];
 
-console.log(web)
+
+
+
+console.log(web);
+
+// empty object
+var a={
+
+};
+a.b=2;
+console.log(a);
+
+//check in object
+var c="b" in a;
+console.log(c);
