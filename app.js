dc24bc321a214658ae81154a263d6ce0d46cc4e4
fahadkhan917 <fk16181@gmail.com>
2020-07-24 08:51:25 +1000

    Update app.js

--- app.js
+++ app.js
@@ -2,29 +2,46 @@
 
 // array return object
 
-var web={
-//propertie:value    
-    t1:"ali",
-    t2:"fahad",
-    t3:"asad",
-    t5:['umair','zain']
-};
-//add a t4 like this
-web.t4="umair";
-web.t6=['huma','areeb'];
+// var web={
+// //propertie:value    
+//     t1:"ali",
+//     t2:"fahad",
+//     t3:"asad",
+//     t5:['umair','zain']
+// };
+// //add a t4 like this
+// web.t4="umair";
+// web.t6=['huma','areeb'];
 
 
 
 
-console.log(web);
+// console.log(web);
 
-// empty object
-var a={
+// // empty object
+// var a={
 
-};
-a.b=2;
-console.log(a);
+// };
+// a.b=2;
+// console.log(a);
 
-//check in object
-var c="b" in a;
-console.log(c);
+// //check in object
+// var c="b" in a;
+// console.log(c);
+
+
+var myquestion=[
+{
+question:"what is your name",
+answer:{
+a:"fahad",
+b:"ali",
+c:"umer"
+
+},
+correctanswer:"c"
+
+}
+
+
+]
